b88e2012fe939830c66e1df1028b22a6a66f76e3
Pedro Eduardo Loayza huaman <pfloayza1704@gmail.com>
2025-09-19 12:03:11 -0500

    usuario,categoria,marca,cliente-usuarip beta,.jar_2

--- Farma_FX_2025/src/main/java/pe/edu/upeu/farmafx/control/MenuClienteController.java
+++ Farma_FX_2025/src/main/java/pe/edu/upeu/farmafx/control/MenuClienteController.java
@@ -6,18 +6,27 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import pe.edu.upeu.farmafx.FarmaFXApplication;
+import pe.edu.upeu.farmafx.enums.Estado;
+import pe.edu.upeu.farmafx.modelo.Categoria;
 import pe.edu.upeu.farmafx.modelo.Usuario;
+import pe.edu.upeu.farmafx.servicio.CategoriaServicioI;
 import pe.edu.upeu.farmafx.utils.NavegadorVistas;
 import pe.edu.upeu.farmafx.utils.Vistas;
+import java.util.List;
 
 @Controller
 public class MenuClienteController {
 
     @Autowired
     private NavegadorVistas navegador;
+
+    @Autowired
+    private CategoriaServicioI categoriaServicio;
 
     @FXML
     private BorderPane panelPrincipal;
@@ -26,11 +35,24 @@ public class MenuClienteController {
     private Label dniUsuarioLabel;
 
     @FXML
+    private Menu menuCategorias;
+
+    @FXML
     public void initialize() {
         Usuario usuario = FarmaFXApplication.usuarioLogueado;
         if (usuario != null) {
             dniUsuarioLabel.setText("Usuario: " + usuario.getDni());
         }
+        List<Categoria> categorias = categoriaServicio.listarCategorias();
+        categorias.stream()
+                .filter(c -> c.getEstado() == Estado.ACTIVO)
+                .forEach(categoriaActiva ->{
+                    MenuItem itemCategoria = new MenuItem(categoriaActiva.getNombre());
+                    itemCategoria.setDisable(true);
+                    if (menuCategorias != null) {
+                        menuCategorias.getItems().add(itemCategoria);
+                    }
+                });
     }
 
     @FXML
